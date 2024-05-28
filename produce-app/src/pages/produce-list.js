231671d10d7cba717c7914f69b0e69f8ef9af375
Alfredo Iniguez <alfredo.prog@gmail.com>
2024-05-27 23:29:10 -0700

    In the Confirm Your Order page it displays the case quantity and the total. When user clicks on the Edit button, TextBox's appear for the name of produce item, and quantity so the user could change the value for produce item, and quantity.  When user clicks on the edit button, the edit button is replaced by the save button, and the delete button is replaced by the cancel button.

--- produce-app/src/pages/produce-list.js
+++ produce-app/src/pages/produce-list.js
@@ -8,6 +8,19 @@ const ProduceList = () => {
   const router = useRouter();
   const { order } = router.query;
   const [produceItems, setProduceItems] = useState([])
+  const [editIndex, setEditIndex]=useState(null)
+  const [qtyIndex, setQtyIndex]=useState(null)
+  const [value, setValue]=useState(null)
+  const [valueQty, setValueQty]=useState(null)
+  const [editButtonIndex, setEditButtonIndex]=useState(null)
+  const [deleteButtonIndex, setDeleteButtonIndex]=useState(null)
+
+
+  let quantityItems=[]
+  let total_Balance=[]
+  let grand_Total=[]
+  let total=0.000
+  let totalQ=0
 
   useEffect(()=>{
     console.log(order)
@@ -18,54 +31,227 @@ const ProduceList = () => {
   
   }, [order])
 
+  const getQuantity=()=>{
+    let index=0;
+    let currentQuantity=0;
+
+    while (produceItems.length > index) {
+      quantityItems[index]=produceItems[index].Qty;
+      index++;
+    }//end of while loop
+
+    console.log(quantityItems)
+  }
+
+  const getTotalQuantity=()=>{
+    let index=0
+    let tempValue1=0
+    let tempValue2=0
+    let tempTotalQnty=0
+
+    getQuantity();
+
+    while (quantityItems.length > index) {
+      tempValue1=quantityItems[index]
+      tempTotalQnty=tempValue1+tempValue2
+      tempValue2=tempTotalQnty
+      index++
+    }//end of while loop
+
+    totalQ=tempTotalQnty
+
+    console.log(tempTotalQnty)
+    return totalQ
+
+  }
+  
+
+  const calculateTotal=()=>{
+    let tb=0
+    let index=0;
+    let tempPrice
+    let tempQnty
+    let tempTotal=0
+    let tl=0
+    let x=0
+    let temp=0.00
+    let temp2=0.00
+    let currentTotal=0.00
+
+    while (produceItems.length > index) {
+      tempPrice=total_Balance[index];
+      tempQnty=quantityItems[index];
+
+      console.log("tempPrice is ", tempPrice, ",and the Quantity is ", tempQnty)
+
+      tempTotal= tempPrice*tempQnty
+      grand_Total[index]=tempTotal;
+      index++
+    }//end of while loop
+    console.log(grand_Total)
+
+    while (grand_Total.length > x) {
+      temp=grand_Total[x]
+      console.log("BEFORE ADDITION: temp is ", temp, "temp2 is ",temp2, "total is ", total)
+      total=temp+temp2
+      console.log("currentTotal is ", total)
+      temp2=total
+      console.log("AFTER ADDITION: temp is ", temp, "temp2 is ",temp2, "currentTotal is ", total)
+      x++
+    }//end of while loop
+
+    console.log("total is ", total)
+  }
+
+  const getTotal=()=>{
+
+    let index=0
+    let qnty=0
+
+    while(produceItems.length > index){
+
+      console.log(produceItems[index])
+
+      if(produceItems[index].promo_price===0){
+        total_Balance[index]=produceItems[index].case_cost
+      }
+      else{
+        total_Balance[index]=produceItems[index].promo_price
+      }
+      index++;
+    }//end of while loop
+
+    //TODO: AT THIS POINT WE HAVE ALL THE CASE COST AND PROMO COST IN total_Balance ARRAY.
+    //TODO: NOW LETS GET THE QUANTITY FOR EACH PRODUCE ITEM.
+    getQuantity()
+    //TODO:CALCULATE TOTAL
+    calculateTotal();
+
+    console.log("Total quanity is ", qnty)
+    console.log(total_Balance)
+    return total
+  }
+
+  const toggleEdit=(index)=>{
+    console.log("index is ", index, ", and editIndex is ", editIndex)
+
+    setQtyIndex(index !== qtyIndex ? index : null)    
+    setEditIndex(index !== editIndex ? index : null)
+    setEditButtonIndex(index !== editButtonIndex ? index : null)
+    setDeleteButtonIndex(index !== deleteButtonIndex ? index : null)
+  }
+
+  const setValueUpdateProduce=()=>{
+
+    console.log("Inside value of setValueUpdateProduce is")
+  }
+
+  const setValueUpdateQty=()=>{
+    console.log("Inside setValueUpdateQty")
+  }
 
   return (
     <div className="grid grid-rows-1 border border-black bg-blue-50">
         <div className='grid grid-rows-1'>
             <div className='flex justify-center'>
+              {/* {console.log(produceItems[length])} */}
                 <h1 className="text-2xl font-bold mb-4">Confirm Your Order</h1>
             </div>
         </div>
       <div>
         {produceItems.map((item, index) => (
-          <div key={index} className="mb-2">
+          <div key={item.id} className="mb-2">
               <div className="flex justify-center items-center">
                 <div className='border border-black w-8/12 grid grid-cols-5 rounded-md p-1 bg-amber-50'>
                   <div className='grid grid-cols-1'>
                       <div className='flex justify-center'>
-                          <p className='uppercase font-bold text-sm font-instrument'>{item.name}</p>
+                        {editIndex===index ?
+                          <TextField 
+                            label="Item"
+                            value={value}
+                            onChange={setValueUpdateProduce}
+                          
+                          />
+                            :
+                            <p className='uppercase font-bold text-sm font-instrument'>{item.name}</p>
+
+                        } 
+
                       </div>
                   </div>
                   <div className='grid grid-cols-1'>
                       <div className='flex justify-center'>
+                        {qtyIndex===index ?
+                        <TextField
+                          label="Qty"
+                          value={valueQty}    
+                          onChange={setValueUpdateQty}
+                        />
+                        :
                           <p className='uppercase font-bold text-sm font-instrument'>Quantity: {item.Qty}</p>
+                        }
                       </div>
                   </div>
                   <div className='grid grid-cols-1'>
                       <div className='flex justify-center'>
-                          <p className='uppercase font-bold text-sm font-instrument'>Price: ${item.promo_price}</p>
+                          <p className='uppercase font-bold text-sm font-instrument'>Price: ${item.promo_price===0 ? item.case_cost : item.promo_price}</p>
                       </div>
                   </div>
                   <div className='grid grid-cols-1'>
                       <div className='flex justify-center'>
-                          <Button className='border border-black'
-                              sx={{
-                                  fontSize: '0.75rem', // smaller font size
-                                  padding: '4px 10px', // custom padding
-                                  minWidth: '42px', // minimum width
-                                  height: '30px', // specific height
-                                  background: '#28a745',
-                                  color:'black'
-                                }}
+                        {editButtonIndex===index ?
+                          <Button className='border border-black' onClick={()=>toggleEdit(index)}
+                          sx={{
+                              fontSize: '0.75rem', // smaller font size
+                              padding: '4px 10px', // custom padding
+                              minWidth: '42px', // minimum width
+                              height: '30px', // specific height
+                              background: '#28a745',
+                              color:'black'
+                            }}
+                            variant='outlined' 
+                            color='primary' 
+                            size='small' 
+                            >SAVE</Button> 
+                              :
+
+                            <Button className='border border-black' onClick={()=>toggleEdit(index)}
+                            sx={{
+                                fontSize: '0.75rem', // smaller font size
+                                padding: '4px 10px', // custom padding
+                                minWidth: '42px', // minimum width
+                                height: '30px', // specific height
+                                background: '#28a745',
+                                color:'black'
+                              }}
                               variant='outlined' 
                               color='primary' 
                               size='small' 
-                          >Edit</Button>
+                            >Edit</Button>
+
+                        
+                        }
+
                       </div>
                   </div>
                   <div className='grid grid-cols-1'>
                       <div className='flex justify-center'>
-                          <Button className='border border-black'
+                        {deleteButtonIndex===index ?
+                            <Button className='border border-black'
+                            sx={{
+                                fontSize: '0.75rem', // smaller font size
+                                padding: '2px 8px', // custom padding
+                                minWidth: '42px', // minimum width
+                                height: '30px' ,// specific height
+                                background: '#FF6347',
+                                color: 'white'
+                              }}
+                                variant='outlined' 
+                                color='primary' 
+                                size='small' 
+                                >CANCEL</Button>
+                              :
+                              <Button className='border border-black'
                               sx={{
                                   fontSize: '0.75rem', // smaller font size
                                   padding: '2px 8px', // custom padding
@@ -74,17 +260,26 @@ const ProduceList = () => {
                                   background: '#FF6347',
                                   color: 'white'
                                 }}
-                              variant='outlined' 
-                              color='primary' 
-                              size='small' 
-                          
-                          >Delete</Button>
+                                  variant='outlined' 
+                                  color='primary' 
+                                  size='small' 
+                              
+                                  >Delete</Button>
+                        
+                        }
+
                       </div>
                   </div>
                 </div>
               </div>
           </div>
         ))}
+      </div>
+      <div className='flex justify-center'>
+        <p className='font-bold font-instrument'>CASE QUANTITY:{getTotalQuantity()}</p>
+      </div>
+      <div className='flex justify-center mb-4'>
+        <p className='font-bold font-instrument'>TOTAL:${parseFloat(getTotal()).toFixed(2)}</p>
       </div>
       <div className='flex justify-center'>
         <Button className='border border-black mb-2'
@@ -97,9 +292,6 @@ const ProduceList = () => {
             color: 'white'
         }}
         >Send Order</Button>
-      </div>
-      <div className='flex justify-center mb-4'>
-        <p>TOTAL:</p>
       </div>
     </div>
   );
--- produce-app/src/pages/produceorder.js
+++ produce-app/src/pages/produceorder.js
@@ -25,30 +25,30 @@ import watermelon from "../components/images/watermelon.png"
 
 
 import { useState, useEffect } from 'react'
-import { toggle } from '@nextui-org/react'
+import { toggle, user } from '@nextui-org/react'
 
 // import {  Autocomplete,  AutocompleteSection,  AutocompleteItem} from "@nextui-org/autocomplete";
 export default function produceorder() {
 
   const router = useRouter();
   
-    const [produceListItems, setProduceListItems] = useState([{id:0, name:"apple gala", product_code:"110", inventory:"40", case_cost:27.41, case_size:"40 lbs", promo_price:17.49, stock:true, produce_Image: gala_apple, Qty:1},
-                            {id:1, name:"apple fuji", product_code:"108", inventory:"20", case_cost:27.41, case_size:"40 lbs", promo_price:0, stock:true, produce_Image: fuji_apple, Qty:1},
-                            {id:2, name:"apple honeycrisp", product_code:"111", inventory:"80", case_cost:27.41, case_size:"40 lbs", promo_price:12.49, stock:true, produce_Image: honey_crisp, Qty:1},
-                            {id:3, name:"apple granny smith", product_code:"114", inventory:"10", case_cost:49.50, case_size:"40 lbs", promo_price:0, stock:false, produce_Image: granny_smith, Qty:1},
-                            {id:4, name:"oranges", product_code:"350", inventory:"22", case_cost:29.41, case_size:"38 lbs", promo_price:0, stock:true, produce_Image: oranges_navel, Qty:1},
-                            {id:5, name:"lemons", product_code:"266", inventory:"8", case_cost:11.19, case_size:"75 units", promo_price:0, stock:false, produce_Image: lemons, Qty:1},
-                            {id:6, name:"limes", product_code:"278", inventory:"65", case_cost:51.49, case_size:"230 units", promo_price:0, stock:true, produce_Image: limes, Qty:1},
-                            {id:7, name:"strawberries", product_code:"266", inventory:"100", case_cost:22.99, case_size:"8 units", promo_price:0, stock:true, produce_Image: strawberries, Qty:1},
-                            {id:8, name:"bananas", product_code:"142", inventory:"445", case_cost:40.41, case_size:"40 lbs", promo_price:0, stock:true, produce_Image: bananas, Qty:1},
-                            {id:9, name:"blueberries", product_code:"166", inventory:"110", case_cost:21.41, case_size:"10 units", promo_price:11.00, stock:true, produce_Image: blueberries, Qty:1},
-                            {id:10, name:"cabbage", product_code:"178", inventory:"12", case_cost:16.00, case_size:"45 lbs", promo_price:0, stock:false, produce_Image: cabbage, Qty:1},
-                            {id:11, name:"green grapes", product_code:"248", inventory:"55", case_cost:53.49, case_size:"18 lbs", promo_price:0, stock:true, produce_Image: green_grapes, Qty:1},
-                            {id:12, name:"raspberries", product_code:"167", inventory:"10", case_cost:25.99, case_size:"10 unit s", promo_price:0, stock:false, produce_Image: raspberries, Qty:1},
-                            {id:13, name:"red grapes", product_code:"250", inventory:"50", case_cost:53.49, case_size:"18 lbs", promo_price:0, stock:true, produce_Image: red_grapes, Qty:1},
-                            {id:14, name:"roma", product_code:"482", inventory:"100", case_cost:25.49, case_size:"25 lbs", promo_price:15.00, stock:true, produce_Image: roma, Qty:1},
-                            {id:15, name:"tomato", product_code:"478", inventory:"40", case_cost:20.49, case_size:"15 lbs", promo_price:0, stock:true, produce_Image: tomato, Qty:1},
-                            {id:16, name:"watermelon", product_code:"304", inventory:"40 bins", case_cost:200.15, case_size:"120 units", promo_price:0, stock:true, produce_Image: watermelon, Qty:1},
+    const [produceListItems, setProduceListItems] = useState([{id:0, name:"apple gala", product_code:"110", inventory:"40", case_cost:27.41, case_size:"40 lbs", promo_price:17.49, stock:true, produce_Image: gala_apple, Qty:1,totalBalance:0.00},
+                            {id:1, name:"apple fuji", product_code:"108", inventory:"20", case_cost:27.41, case_size:"40 lbs", promo_price:0, stock:true, produce_Image: fuji_apple, Qty:1,totalBalance:0.00},
+                            {id:2, name:"apple honeycrisp", product_code:"111", inventory:"80", case_cost:27.41, case_size:"40 lbs", promo_price:12.49, stock:true, produce_Image: honey_crisp, Qty:1,totalBalance:0.00},
+                            {id:3, name:"apple granny smith", product_code:"114", inventory:"10", case_cost:49.50, case_size:"40 lbs", promo_price:0, stock:false, produce_Image: granny_smith, Qty:1,totalBalance:0.00},
+                            {id:4, name:"oranges", product_code:"350", inventory:"22", case_cost:29.41, case_size:"38 lbs", promo_price:0, stock:true, produce_Image: oranges_navel, Qty:1,totalBalance:0.00},
+                            {id:5, name:"lemons", product_code:"266", inventory:"8", case_cost:11.19, case_size:"75 units", promo_price:0, stock:false, produce_Image: lemons, Qty:1,totalBalance:0.00},
+                            {id:6, name:"limes", product_code:"278", inventory:"65", case_cost:51.49, case_size:"230 units", promo_price:0, stock:true, produce_Image: limes, Qty:1,totalBalance:0.00},
+                            {id:7, name:"strawberries", product_code:"266", inventory:"100", case_cost:22.99, case_size:"8 units", promo_price:0, stock:true, produce_Image: strawberries, Qty:1,totalBalance:0.00},
+                            {id:8, name:"bananas", product_code:"142", inventory:"445", case_cost:40.41, case_size:"40 lbs", promo_price:0, stock:true, produce_Image: bananas, Qty:1,totalBalance:0.00},
+                            {id:9, name:"blueberries", product_code:"166", inventory:"110", case_cost:21.41, case_size:"10 units", promo_price:11.00, stock:true, produce_Image: blueberries, Qty:1,totalBalance:0.00},
+                            {id:10, name:"cabbage", product_code:"178", inventory:"12", case_cost:16.00, case_size:"45 lbs", promo_price:0, stock:false, produce_Image: cabbage, Qty:1,totalBalance:0.00},
+                            {id:11, name:"green grapes", product_code:"248", inventory:"55", case_cost:53.49, case_size:"18 lbs", promo_price:0, stock:true, produce_Image: green_grapes, Qty:1,totalBalance:0.00},
+                            {id:12, name:"raspberries", product_code:"167", inventory:"10", case_cost:25.99, case_size:"10 unit s", promo_price:0, stock:false, produce_Image: raspberries, Qty:1,totalBalance:0.00},
+                            {id:13, name:"red grapes", product_code:"250", inventory:"50", case_cost:53.49, case_size:"18 lbs", promo_price:0, stock:true, produce_Image: red_grapes, Qty:1,totalBalance:0.00},
+                            {id:14, name:"roma", product_code:"482", inventory:"100", case_cost:25.49, case_size:"25 lbs", promo_price:15.00, stock:true, produce_Image: roma, Qty:1,totalBalance:0.00},
+                            {id:15, name:"tomato", product_code:"478", inventory:"40", case_cost:20.49, case_size:"15 lbs", promo_price:0, stock:true, produce_Image: tomato, Qty:1,totalBalance:0.00},
+                            {id:16, name:"watermelon", product_code:"304", inventory:"40 bins", case_cost:200.15, case_size:"120 units", promo_price:0, stock:true, produce_Image: watermelon, Qty:1,totalBalance:0.00},
                           ]);
     const [userCurrentOrder, setUserCurrentOrder]=useState([]);
     const [value, setValue]=useState(null);
@@ -270,7 +270,13 @@ export default function produceorder() {
   }
 
   const handleConfirmOrder=()=>{
+  
+
     console.log(userCurrentOrder)
+    
+    console.log(parseFloat(totalBalance).toFixed(2))
+
+
         // Convert the order to a query parameter string
         const query = encodeURIComponent(JSON.stringify(userCurrentOrder));
 
@@ -280,7 +286,6 @@ export default function produceorder() {
   }
 
   return (
-
     <div>
       {console.log("currentBalance is ", currentBalance)}
       <div className="flex justify-center">
